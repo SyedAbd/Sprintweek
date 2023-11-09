fea881d6d86c0c6d882865b6b9e19721bcde1bda
Thenya Kamau <thenyakamau@gmail.com>
2023-11-09 11:49:18 +0200

    add background

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rigidBody;
+    private CapsuleCollider2D collide;
+    private SpriteRenderer spriteRenderer;
+    //private Animator animator;
+
+    [SerializeField] private LayerMask jumpableGround;
+    [SerializeField] private AudioSource jumpSoundEffect;
+
+    private float directionX = 1f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float jumpForce = 14f;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        collide = GetComponent<CapsuleCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        //animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float velocityY = rigidBody.velocity.y;
+        float velocityX = rigidBody.velocity.x;
+
+        float newPosition = directionX * moveSpeed;
+        rigidBody.velocity = new Vector2(newPosition, velocityY);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rigidBody.velocity = new Vector2(velocityX, jumpForce);
+            //jumpSoundEffect.Play();
+        }
+
+    }
+
+    private bool IsGrounded()
+    {
+        Bounds bounds = collide.bounds;
+        return Physics2D.BoxCast(bounds.center, bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+}
