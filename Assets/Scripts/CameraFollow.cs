5f709719a70ae8e07549e3f9095f509b1daaf757
shah <syedabdullahamjad@gmail.com>
2023-11-10 03:20:21 +0200

    changed movement

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+     public float FollowSpeed = 2f;
+    public float yOffset = 1f;
+    private float xOffset = 20f;
+    public float speed = 5f;
+
+    public Transform target;
+
+    // Update is called once per frame
+    void Start()
+    {
+        Vector3 newPos = new Vector3(target.position.x + xOffset, target.position.y + yOffset, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+    }
+    void Update()
+    {
+        Vector3 newPos = new Vector3(transform.position.x + speed, target.position.y + yOffset, -10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+    }
+}
