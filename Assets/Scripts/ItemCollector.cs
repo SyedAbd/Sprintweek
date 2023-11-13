0825b74a1fa15ff1c2036815e0ed25e429261993
Thenya Kamau <thenyakamau@gmail.com>
2023-11-13 23:42:08 +0200

    add spray bottles prefab and scene

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int sprayCount = 0;
+    [SerializeField] private Text sprayText;
+
+    [SerializeField] private AudioSource collectionSoundEffect;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        GameObject obj = collision.gameObject;
+        if (obj.CompareTag("Spray"))
+        {
+            //collectionSoundEffect.Play();
+            Destroy(obj);
+            sprayCount++;
+
+            sprayText.text = "Sprays: " + sprayCount;
+        }
+    }
+}
