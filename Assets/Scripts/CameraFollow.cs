507a988277052f4a11cb4e6216736d8beecb772d
shah <syedabdullahamjad@gmail.com>
2023-11-13 15:37:17 +0200

    Camera script updated

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -8,34 +6,47 @@ public class CameraFollow : MonoBehaviour
     public float FollowSpeed = 2f;
     public float yOffset = 2.7f;
     public float xOffset = 6f;
-    public float speed = 10f;
+    public float speed = 1.5f;
 
     public Transform target;
 
-    // Update is called once per frame
     void Start()
     {
-        Vector3 newPos = new Vector3(target.position.x + xOffset, target.position.y + yOffset, -10f);
-        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
-    }
-    void Update()
-    {
-        //Vector3 newPos = new Vector3(transform.position.x + speed, target.position.y + yOffset, -10f);
-        //transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
-        Vector3 newPos = new Vector3(target.position.x + xOffset, target.position.y + yOffset, -10f);
-        transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+        InitializeCameraPosition();
     }
 
-    void OnTriggerExit2D(Collider2D collision) {
+    void Update()
+    {
+        UpdateCameraPosition();
+    }
+
+    private void InitializeCameraPosition()
+    {
+        Vector3 newPos = new Vector3(target.position.x + xOffset, target.position.y + yOffset, -10f);
+        transform.position = newPos;
+    }
+
+    private void UpdateCameraPosition()
+    {
+        float targetX = GetX();
+        Vector3 newPos = new Vector3(targetX, target.position.y + yOffset, -10f);
+        transform.position = Vector3.Lerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
+    }
+
+    private float GetX()
+    {
+        if ((transform.position.x + speed) < target.position.x)
+        {
+            return transform.position.x + speed + 0.1f;
+        }
+        return transform.position.x + speed;
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
         if (collision.CompareTag("Player"))
         {
             SceneManager.LoadScene(1);
         }
-
-
-
-
     }
-
-
 }
