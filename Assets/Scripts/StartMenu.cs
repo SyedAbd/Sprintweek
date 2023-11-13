2c024c387400843789e77b39b8b1a04b64812971
kelethe <kelethe@outlook.com>
2023-11-13 23:25:58 +0200

    StartMenu & EndScene
    StartMenu & EndScene

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
